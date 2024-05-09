2ca78d5c00d884dfef881900ff651543a881fb56
Janos Feher <feherjani96@gmail.com>
2024-05-09 20:35:54 +0200

    fix link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,14 @@
 import { LayoutGrid } from "@/components/LayoutGrid";
 import Image from "next/image";
 import Muszak from "@/logos/muszak.png"
+import Link from "next/link";
 
 export function Footer() {
   return <footer className="bg-gray-darkest py-14 laptop:py-28 px-5 laptop:px-16 w-full font-display">
     <LayoutGrid>
 
       <div className="flex flex-col space-y-3 laptop:col-span-4 col-start-1 text-xl font-extrabold">
-        <a href="/">NYATA 2024</a>
+        <Link href="/">NYATA 2024</Link>
         <a href="/adatkezelesi.pdf">Adatkezelési tájékoztató</a>
         <a href="https://szakkoli.hu" target="_blank" rel="noopener noreferrer">szakkoli.hu</a>
         {/* <a href="https://muszak.bme.hu" target="_blank" rel="noopener noreferrer">muszak.bme.hu</a>*/}
