0d9795d982122ba808e234e3ca8499884ad0a9f7
Janos Feher <feherjani96@gmail.com>
2024-05-14 18:38:04 +0200

    fix programs graphic position

--- src/components/Programs.tsx
+++ src/components/Programs.tsx
@@ -14,7 +14,7 @@ export function Programs() {
         <p className="laptop:pr-96">Az idei NYATA témája a <strong>Technológia és Társadalom</strong>. Mik a legújabb <strong>technológiai trendek</strong>, mik ezeknek a társadalmi vonatkozásai? Hogyan használjuk hatékonyan az <strong>AI</strong> adta lehetőségeket? Hogyan tud <strong>fennmaradni egy város, ország, társadalom</strong> a 21.században?Ha érdekelnek ezek a kérdések, csatlakozz hozzánk a 27. alkalommal megrendezésre kerülő NYATA-n és vitassuk meg közösen a válaszokat! <strong>Találkozunk Sopronban!</strong></p>
       </Box>
 
-      <div className="absolute right-[30px] -top-1 laptop:-top-24 hidden tablet:block">
+      <div className="absolute laptop:-top-24 hidden tablet:block tablet:-top-6 tablet:right-0 laptop:right-7">
         <People className="tablet:w-[264px] tablet:h-[139px] laptop:w-[528px] laptop:h-[278px]" />
       </div>
     </Section>
