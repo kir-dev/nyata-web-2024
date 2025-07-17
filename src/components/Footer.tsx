c62947a184b318ae9ca470353b6bb56fc943879d
Lipták Péter <liptak.peti.98@gmail.com>
2025-07-17 10:51:14 +0200

    Updates footer for NYATA 2025 with new branding and organizer details

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,15 +8,21 @@ export function Footer() {
     <LayoutGrid>
 
       <div className="flex flex-col space-y-3 laptop:col-span-4 col-start-1 text-xl font-extrabold">
-        <Link href="/">NYATA 2024</Link>
+        <Link href="/">NYATA 2025</Link>
         <a href="/adatkezelesi.pdf">Adatkezelési tájékoztató</a>
         <a href="https://szakkoli.hu" target="_blank" rel="noopener noreferrer">szakkoli.hu</a>
         {/* <a href="https://muszak.bme.hu" target="_blank" rel="noopener noreferrer">muszak.bme.hu</a>*/}
 
       </div>
       <div className="flex flex-col items-end text-right col-span-4 col-start-10 space-y-3 text-base font-extrabold">
-        <div className="mr-[-9px]"><Image src={Muszak} width={102} height={102} alt="MŰSZAK logo" /></div>
-        <div>A 2024-es NYATA-t a Műegyetemi<br /> Szakkollégiumok Közössége (MŰSZAK) szervezi.</div>
+        <div className="mr-[-9px]"><Image src={"/Interkoll_logo.png"} width={102} height={102} alt="Interkoll logo" /></div>
+        <div>A NYATA 2025 összszakkollégiumi szervezésben valósult meg.<br/>
+          <strong>Főszervezők:</strong><br/>
+          Balogh András György<br/>
+          Guezodje Kristóf<br/>
+          Gyuricza Mátyás<br/>
+          Szalai Sándor
+        </div>
 
       </div>
     </LayoutGrid>
